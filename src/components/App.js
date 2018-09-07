de0e0ebc532b2366d8b909008ea2a117312c69d2
Pooya Golchian <pooya.golchian@gmail.com>
2018-09-07 14:00:35 +0430

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+      <h1>Recipe Finder</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
